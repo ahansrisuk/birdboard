0b4c3ccb52097caeebb765c9b285d1952de8b95c
Allen Hansrisuk <ahansrisuk@email.com>
2019-12-23 12:39:09 -0500

    model tests

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -15,8 +15,6 @@ class ProjectsTest extends TestCase
     public function a_user_can_create_a_project()
     {
 
-        $this->withoutExceptionHandling();
-
         $attributes = [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph
@@ -25,6 +23,19 @@ class ProjectsTest extends TestCase
         $this->post('/projects', $attributes)->assertRedirect('/projects');
         $this->assertDatabaseHas('projects', $attributes);
         $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    /** @test */
+    public function a_user_can_view_a_project()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $project = factory(Project::class)->create();
+
+        $this->get($project->path())
+            ->assertSee($project->title)
+            ->assertSee($project->description);
     }
 
     /** @test */
